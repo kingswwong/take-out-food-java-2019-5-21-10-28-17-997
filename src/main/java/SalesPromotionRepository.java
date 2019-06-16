2cbd34d98ccda98f703381b99da4dd85846b6c8a
kingswwong <467271883@qq.com>
2019-06-16 23:53:44 +0800

    Add bestChageImpl

--- src/main/java/SalesPromotionRepository.java
+++ src/main/java/SalesPromotionRepository.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public interface SalesPromotionRepository {
     List<SalesPromotion> findAll();
+
+    ArrayList bestCharge(ArrayList<Item> items, double sum);
 }
--- src/test/java/ItemRepositoryTestImpl.java
+++ src/test/java/ItemRepositoryTestImpl.java
@@ -1,8 +1,26 @@
 import java.util.List;
 
+
 public class ItemRepositoryTestImpl implements ItemRepository{
     @Override
     public List<Item> findAll() {
         return TestData.ALL_ITEMS;
+    }
+
+
+    /**
+     * Find Item by Id
+     * @param id
+     * @author Dean Huang
+     */
+    @Override
+    public Item getItemById(String id) {
+        //TODO: Add Elemnet not found Exception
+        for(Item item: TestData.ALL_ITEMS){
+            if (item.getId().equals(id)){
+                return item;
+            }
+        }
+        return null;
     }
 }
